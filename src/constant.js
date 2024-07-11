15cd85e4ea7c77e97880e59fee9013230b5f0e9b
afandylatodjo <ahmadafandy660@gmail.com>
2024-07-11 08:02:34 +0800

    First Commit

---
+++ src/constant.js
@@ -0,0 +1,40 @@
+export const scaleFactor = 4;
+
+
+const github = "https://github.com/afandylatodjo";
+const tv = "https://www.youtube.com/@TsodingDaily";
+const degree = "https://drive.google.com/file/d/1qVyOAEiMwMCf3Kt6eF39N2_fRfdqBWs6/view?usp=drive_link";
+const paper = "https://drive.google.com/file/d/1am6RzOX8nDKdCgv3LsrVybhNi1pI_rVf/view?usp=drive_link";
+const inflearn = "https://www.infinitelearning.id/";
+
+export const dialogueData = {
+  pc: `
+  This is my PC, my powerfull tool to create some Website and Mobile Project. I work mostly in Javasript, PHP, MySQL, React.js, Express.js when making Website Project, i prefer Backend when working in team. When making Mobile Project I choose Java, Dart(Flutter) and Kotlin. Here is my <a href={github} targe="_">Github</a> page!.
+  `,
+  tv: `
+   This is my TV, I usually watching my favourite recreational programming channel
+   <a href={channel} target="_">Tsoding Daily</a>. Really recommended if you like same type of channel.
+  `,
+  "cs-degree": `
+  <a href={degree}>This</a> is my MSIB Certificate after completing Independent Study Program Batch 5 on Web Development at <a href={inflearn} target="_">Infinite Learning</a> in year of 2023.
+  `,
+  "sofa-table":`
+  This is my Sofa. I always take a sit here and relaxing myself while watching YouTube.
+  `,
+  bed: `
+  This is my Bed. I sleep here (sometimes).
+  `,
+  resume:`
+  This is my Desk and on top of it is my <a href={paper} target="_">Resume</a>.
+  `,
+  project:`
+  Pending
+  `,
+  library:`
+  There are a lot of books here, some is programming and some is manga.
+  `,
+  exit:`
+  Exit? Just close the tab if you will.
+  `
+}
+
